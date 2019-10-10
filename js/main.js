8df2ffc226de30dbf1f6c85cf9174b763e06420c
Brandon Caples <brandoncaples@gmail.com>
2019-10-09 22:47:24 -0500

    Updated layout and header

--- js/main.js
+++ js/main.js
@@ -1,22 +1,36 @@
-var elements = document.getElementsByTagName('script')
+// Show/Hide the mobile navigation
+( function() {
+	var navTrigger = document.getElementById( 'nav-trigger' );
+	var navWrapper = document.getElementById( 'nav-wrapper' );
 
-Array.prototype.forEach.call(elements, function(element) {
-  if (element.type.indexOf('math/tex') != -1) {
-     // Extract math markdown
-     var textToRender = element.innerText || element.textContent;
+	navTrigger.addEventListener( 'click', function() {
+		if ( navWrapper.className == 'hide' ) {
+			navWrapper.className = navWrapper.className.replace( /\bhide\b/g, 'show' );
+		} else {
+			navWrapper.className = navWrapper.className.replace( /\bshow\b/g, 'hide' );
+		}
+	} );
+}() );
 
-     // Create span for KaTeX
-     var katexElement = document.createElement('span');
+var elements = document.getElementsByTagName( 'script' )
 
-     // Support inline and display math
-     if (element.type.indexOf('mode=display') != -1){
-       katexElement.className += "math-display";
-       textToRender = '\\displaystyle {' + textToRender + '}';
-     } else {
-       katexElement.className += "math-inline";
-     }
+Array.prototype.forEach.call( elements, function( element ) {
+	if ( element.type.indexOf( 'math/tex' ) != -1 ) {
+		// Extract math markdown
+		var textToRender = element.innerText || element.textContent;
 
-     katex.render(textToRender, katexElement);
-     element.parentNode.insertBefore(katexElement, element);
-  }
-});
+		// Create span for KaTeX
+		var katexElement = document.createElement( 'span' );
+
+		// Support inline and display math
+		if ( element.type.indexOf( 'mode=display' ) != -1 ){
+			katexElement.className += "math-display";
+			textToRender = '\\displaystyle {' + textToRender + '}';
+		} else {
+			katexElement.className += "math-inline";
+		}
+
+		katex.render( textToRender, katexElement );
+		element.parentNode.insertBefore( katexElement, element );
+	}
+} );
